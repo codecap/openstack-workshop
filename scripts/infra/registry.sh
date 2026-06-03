063675e24bb0eb8dccedffbc5f43bb697eb766bc
Vladislav Nazarenko <vnazarenko@socket.de>
2026-06-03 15:57:59 +0200

    upd: scripts/infra/registry.sh

--- scripts/infra/registry.sh
+++ scripts/infra/registry.sh
@@ -1,43 +1,44 @@
 #!/usr/bin/env bash
-HARBOR_USER=admin
-HARBOR_PASS=Harbor12345
-apt update; apt install docker-compose-v2 -y
-# Install
-wget https://github.com/goharbor/harbor/releases/download/v2.10.0/harbor-online-installer-v2.10.0.tgz
-tar xzvf harbor-online-installer-v2.10.0.tgz
+HARBOR_USER="admin"
+HARBOR_PASS="Harbor12345"
+HARBOR_VERSION="2.10.0"
+
+# Download
+wget https://github.com/goharbor/harbor/releases/download/v${HARBOR_VERSION}/harbor-online-installer-v${HARBOR_VERSION}.tgz
+tar xzvf harbor-online-installer-v${HARBOR_VERSION}.tgz
+
+# Configure
 cd harbor
 cp harbor.yml.tmpl harbor.yml
+
+# set admin password
+sed -i harbor.yml -e "s/^harbor_admin_password:.*/harbor_admin_password: $HARBOR_PASS/"
 # change the hostname
-sed -i harbor.yml -e "s/^hostname:.*/hostname: registry.wrx.sckt.net"
+sed -i harbor.yml -e "s/^hostname:.*/hostname: registry.wrx.sckt.net/"
 # switch off https
-sed -i '/^https:/,/private_key:/ s/^/#/' harbor.yml
+sed -i harbor.yml '/^https:/,/private_key:/ s/^/#/'
+
+# Install
 ./install.sh ; sleep 5;
 
 
-# Set proxy in
-#  * common/config/core/env
-#  * common/config/jobservice/env
-# HTTP_PROXY=https://proxy.wrx.sckt.net:3128
-# HTTPS_PROXY=https://proxy.wrx.sckt.net:3128
-# NO_PROXY=127.0.0.1,localhost,.wrx.sckt.net,harbor-core,harbor-db,harbor-jobservice,registry,registryctl,chartmuseum,trivy,redis
+# Setupt Proxy Repository
+curl -sS -u "$HARBOR_USER:$HARBOR_PASS" -X POST "http://localhost/api/v2.0/registries" \
+ -H "Content-Type: application/json" \
+ -d '{
+   "name": "quay-upstream",
+   "type": "quay",
+   "url": "https://quay.io"
+ }'
 
-
-# # Configure
-# curl -sS -u "$HARBOR_USER:$HARBOR_PASS" -X POST "http://localhost/api/v2.0/registries" \
-#   -H "Content-Type: application/json" \
-#   -d '{
-#     "name": "quay-upstream",
-#     "type": "quay",
-#     "url": "https://quay.io"
-#   }'
-# curl -sS -u "$HARBOR_USER:$HARBOR_PASS" -X POST "http://localhost/api/v2.0/projects" \
-#   -H "Content-Type: application/json" \
-#   -d '{
-#     "project_name": "quay",
-#     "public": true,
-#     "metadata": {
-#       "public": "true",
-#       "proxy_cache": "true"
-#     },
-#     "registry_id": 6
-#   }'
+curl -sS -u "$HARBOR_USER:$HARBOR_PASS" -X POST "http://localhost/api/v2.0/projects" \
+ -H "Content-Type: application/json" \
+ -d '{
+   "project_name": "quay",
+   "public": true,
+   "metadata": {
+     "public": "true",
+     "proxy_cache": "true"
+   },
+   "registry_id": 1
+ }'
